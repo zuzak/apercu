5fb23b28f7fede24538c7708344c30409684a9c1
Douglas Gardner <douglas@chippy.ch>
2013-09-12 14:38:41 +0000

    Add clientside tests; add submit button

---
+++ test/clientside.js
@@ -0,0 +1,36 @@
+var config = require('../config');
+var should = require('should');
+var Browser = require('zombie');
+
+var url = "http://" + config.get("ip") + ":" + config.get('port');
+
+describe('The project list', function(){
+    before(function(done){
+        this.browser = new Browser();
+        this.browser.visit(url).then(done,done);
+    });
+    it("should display the correct repositories", function(done){
+        this.browser.html(".apercu-dummy-passing").should.be.ok;
+        this.browser.html(".apercu-dummy-failing").should.be.ok;
+        this.browser.html(".apercu-dummy-erroring").should.be.ok;
+        this.browser.html(".apercu-dummy-nobuild").should.be.ok;
+        done();
+    });
+    it("should display the correct Travis states", function(done){
+        this.browser.html(".apercu-dummy-passing .label-success").should.be.ok;
+        this.browser.html(".apercu-dummy-failing .label-danger").should.be.ok;
+        this.browser.html(".apercu-dummy-erroring .label-default").should.be.ok;
+        this.browser.html(".apercu-dummy-erroring .label-default").should.be.ok;
+        this.browser.html(".apercu-dummy-nobuild .label").should.not.be.ok;
+        done();
+    });
+    it("should append a repository when the form is submitted",function(done){
+        var browser = this.browser;
+        browser.fill("input","github/gitignore", function(){
+            browser.pressButton("button", function(){
+                browser.html(".github-gitignore").should.be.ok;
+                done();
+            });
+        });
+    });
+});
